7ff21f4eb44ee46ce02d991b58a817f4ecc84cac
lzq <renovil@163.com>
2017-07-14 16:17:54 +0800

    cron job

---
+++ src/main/java/com/lzq/schedule/ScheduleTaskConfig.java
@@ -0,0 +1,32 @@
+package com.lzq.schedule;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * @author lzq
+ * @version 1.0
+ * @description config
+ * @since 2017/7/14 15:08
+ */
+@Configuration
+@ComponentScan("com.lzq.schedule")
+@EnableScheduling
+public class ScheduleTaskConfig implements SchedulingConfigurer {
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        scheduledTaskRegistrar.setScheduler(taskExecutor());
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    public Executor taskExecutor() {
+        return Executors.newScheduledThreadPool(100);
+    }
+}
---
+++ src/main/java/com/lzq/schedule/TaskExecutorConfig.java
@@ -0,0 +1,37 @@
+package com.lzq.schedule;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+/**
+ * @author lzq
+ * @version 1.0
+ * @description config
+ * @since 2017/7/14 15:18
+ */
+@Configuration
+@EnableAsync
+@ComponentScan
+public class TaskExecutorConfig implements AsyncConfigurer {
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(5);
+        taskExecutor.setMaxPoolSize(500);
+        taskExecutor.setQueueCapacity(600);
+        taskExecutor.setKeepAliveSeconds(10);
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return null;
+    }
+}
